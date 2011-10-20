db38ba079e18eef4ca23100ef0d6d601beb591f5
Sheena Luu <sluu@gnm41097.int.gnl>
2011-10-20 14:18:53 +0100

    renaming

--- rss_feed_parser.py
+++ rss_feed_parser.py
@@ -10,10 +10,10 @@ def parseFeed(rssUrl):
     for item in d['items']:
         count=count+1
 
-        articleUrl = item.link.split("&url=")[1]
-        if(articleUrl.startswith("http://www.guardian")):
-            logging.info(articleUrl);
-            logging.info(models.get_article(articleUrl));
-            if(not models.get_article(articleUrl)):
-                models.store_article(datetime.datetime.now(), datetime.datetime.now(), item.title, articleUrl, rssUrl, count,  alerted=False)
+        article_url = item.link.split("&url=")[1]
+        if(article_url.startswith("http://www.guardian")):
+            logging.info(article_url);
+            logging.info(models.get_article(article_url));
+            if(not models.get_article(article_url)):
+                models.store_article(datetime.datetime.now(), datetime.datetime.now(), item.title, article_url, rssUrl, count,  alerted=False)
   
